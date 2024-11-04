b4bfc6a10151a811621e78b0a8a73801ea2ccb70
Daniel-Dominguez-Romero <L24550320@chihuahua2.tecnm.mx>
2024-11-04 17:41:54 -0600

    Add files via upload

---
+++ Eva3_1_Metodos/src/main/java/com/mycompany/eva3_1_metodos/Eva3_1_Metodos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_metodos;
+
+/**
+ *
+ * @author danie
+ */
+public class Eva3_1_Metodos {
+    public static void main(String[] args) {
+      //USO DE LOS METODOS: 
+      //INOVACION O LLAMADA 
+      imprimirMensaje("Hola Mundo!!!"); 
+      
+    }
+    //DECLARACION
+    //header (encabezado) 
+    //IMPRIMIR UN MENSAJE
+    //JAVA: iniciar en minuscula, verbo, siguientes palabras Inicia en Mayusculas 
+    public static void imprimirMensaje (String mensaje) { 
+        System.out.println("TU MENSAJE: " + mensaje);
+    }
+}
